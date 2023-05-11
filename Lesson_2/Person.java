19f84cfdc51b54723cbab5503a98e420781560e6
EvgeniyaZz <zakharova.evgeniya.19@gmail.com>
2023-05-11 16:15:18 +0300

    добавлено задание в Lesson_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "Мужской";
+    String name = "Антон";
+    float height = 1.73f;
+    float weight = 75.5f;
+    int age = 34;
+
+    boolean go() {
+        return true;
+    }
+
+    boolean sit() {
+        return true;
+    }
+
+    boolean run() {
+        return true;
+    }
+
+    String say() {
+        return "saying";
+    }
+
+    void learnJava() {
+            System.out.println(name + "изучает JAVA");
+    }
+}
